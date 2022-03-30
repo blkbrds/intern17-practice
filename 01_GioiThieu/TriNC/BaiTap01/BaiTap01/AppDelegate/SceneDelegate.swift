87fabc3924a6758426029b04b4ec62378b8318ac
Tri Nguyen C <tri.nguyen@monstar-lab.com>
2022-03-30 20:27:00 +0700

    Remove code not use

--- 01_GioiThieu/TriNC/BaiTap01/BaiTap01/AppDelegate/SceneDelegate.swift
+++ 01_GioiThieu/TriNC/BaiTap01/BaiTap01/AppDelegate/SceneDelegate.swift
@@ -7,10 +7,9 @@
 
 import UIKit
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
@@ -20,22 +19,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window.rootViewController = vc
         window.makeKeyAndVisible()
     }
-
-    func sceneDidDisconnect(_ scene: UIScene) {
-    }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-    }
-
-
 }
 
