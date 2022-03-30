41d1eda46c54c521de827c5ef754bb7580dd4cfc
Tri Nguyen C <tri.nguyen@monstar-lab.com>
2022-03-30 22:31:08 +0700

    WIP Implement ex2

---
+++ 04_View/TriNguyenC/BaiTapView/BaiTapView/Scenes/Excersise/Ex2/Excersise02ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  Excersise02ViewController.swift
+//  BaiTapView
+//
+//  Created by tri.nguyen on 30/03/2022.
+//
+
+import UIKit
+
+final class Excersise02ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
