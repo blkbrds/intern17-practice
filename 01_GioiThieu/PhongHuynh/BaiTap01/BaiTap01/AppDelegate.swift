8f724ef64d7583b831c8c6b8e49e02f0ff8000ac
Phong Huynh N. VN.Danang <phong.huynh@monstar-lab.com>
2022-04-04 13:15:28 +0700

    Edit GioiThieu

--- 01_GioiThieu/PhongHuynh/BaiTap01/BaiTap01/AppDelegate.swift
+++ 01_GioiThieu/PhongHuynh/BaiTap01/BaiTap01/AppDelegate.swift
@@ -1,26 +1,11 @@
-//
-//  AppDelegate.swift
-//  BaiTap01
-//
-//  Created by Phong Huynh N. VN.Danang on 3/24/22.
-//
-
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow(frame:  UIScreen.main.bounds)
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
     
-
-
 }
 
--- 01_GioiThieu/PhongHuynh/BaiTap01/BaiTap01/SceneDelegate.swift
+++ 01_GioiThieu/PhongHuynh/BaiTap01/BaiTap01/SceneDelegate.swift
@@ -1,24 +1,16 @@
-//
-//  SceneDelegate.swift
-//  BaiTap01
-//
-//  Created by Phong Huynh N. VN.Danang on 3/24/22.
-//
-
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else {return}
+        
         let window = UIWindow(windowScene: windowScene)
         window.rootViewController = MyViewController()
         self.window = window
         window.makeKeyAndVisible()
-   
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
