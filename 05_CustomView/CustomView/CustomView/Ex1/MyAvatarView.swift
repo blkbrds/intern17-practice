62e6f21279fc29df8ce0a10ffbbef4ebc459b794
Tri Nguyen C <tri.nguyen@monstar-lab.com>
2022-04-06 08:20:53 +0700

    [wip] exercise 01

--- 05_CustomView/CustomView/CustomView/Ex1/MyAvatarView.swift
+++ 05_CustomView/CustomView/CustomView/Ex1/MyAvatarView.swift
@@ -12,30 +12,20 @@ import UIKit
 
 class MyAvatarView: UIView {
     
-    // MARK: - Computed Properties
+    // MARK: - Properties
     
-    private lazy var myAvatarImageView: UIImageView = {
-        let myAvatar = UIImageView()
-        myAvatar.image = UIImage(named: "ic-user")
-        return myAvatar
+    private lazy var avatarImage: UIImage = {
+       let avatar = UIImage()
+       return avatar
     }()
     
-    private lazy var myNameLabel: UILabel = {
-       let myLabel = UILabel()
-        myLabel.textAlignment = .center
-        myLabel.textColor = .orange
-        myLabel.font = UIFont(name: "myname 1", size: 22)
-       return myLabel
-    }()
-    
-    private lazy var myButton: UIButton = {
-        let button = UIButton()
-        button.addTarget(self, action: #selector(tapButtonTouchUpInside), for: .touchUpInside)
-        return button
-    }()
-    
-    // Objc
-    @objc func tapButtonTouchUpInside() {
-        print("tap tap tap")
+    override init(frame: CGRect) {
+        super.init(frame: frame)
     }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
 }
