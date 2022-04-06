7b1dae680097f0a1a33776fdc5e0024cb39023e1
Tinh Bui T <tinh.bui@monstar-lab.com>
2022-04-06 11:48:54 +0700

    Delete file

---
+++ 01_GioiThieu/TinhBT/NoStoryboard/NoStoryboard/HomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewController.swift
+//  NoStoryboard
+//
+//  Created by Tinh Bui T. VN.Danang on 4/6/22.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
