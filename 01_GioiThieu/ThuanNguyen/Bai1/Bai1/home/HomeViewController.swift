2eea6659378a1d2c4bfef0e0ee609640b1c8d37c
Thuan Nguyen <thuan.nguyen@monstar-lab.com>
2022-04-07 16:21:03 +0700

    Create new project

---
+++ 01_GioiThieu/ThuanNguyen/Bai1/Bai1/home/HomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  HomeViewController.swift
+//  Bai1
+//
+//  Created by thuan.nguyen on 07/04/2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func tapButtonTouchUpInside(_ sender: UIButton) {
+        
+        view.backgroundColor = .yellow
+        
+        
+    }
+}
