4f0f9423b5dbb0f51fbf884199d111027286eb70
Phong Huynh N <phong.huynh@monstar-lab.com>
2022-04-08 16:29:24 +0700

    Edit OOP

--- 03_SwiftOOP/PhongHuynh/OOPPlayground.playground/Contents.swift
+++ 03_SwiftOOP/PhongHuynh/OOPPlayground.playground/Contents.swift
@@ -14,25 +14,25 @@ init(tuso: Float, mauso: Float) {
 func congPS(f1: PhanSo, f2:PhanSo) -> Float {
     let Tu = f1.tuSo * f2.mauSo + f2.tuSo * f1.mauSo
     let Mau = f1.mauSo * f2.mauSo
-    return Tu/Mau
+    return Tu / Mau
 }
 
 func truPS(f1: PhanSo, f2:PhanSo) -> Float {
     let Tu = f1.tuSo * f2.mauSo - f2.tuSo * f1.mauSo
     let Mau = f1.mauSo * f2.mauSo
-return Tu/Mau
+return Tu / Mau
 }
 
 func nhanPS(f1: PhanSo, f2:PhanSo) -> Float {
     let Tu = f1.tuSo * f2.tuSo
     let Mau = f1.mauSo * f2.mauSo
-return Tu/Mau
+return Tu / Mau
 }
 
 func chiaPS(f1: PhanSo, f2:PhanSo) -> Float {
     let Tu = f1.tuSo * f2.mauSo
     let Mau = f1.mauSo * f2.tuSo
-return Tu/Mau
+return Tu / Mau
 }
 let ps1 = PhanSo(tuso: 3, mauso: 4)
 let ps2 = PhanSo(tuso: 5, mauso: 4)
@@ -54,9 +54,6 @@ class DaGiac {
             cv = cv + i
         }
         return cv
-    }
-    func inRaCanhDaGiac() {
-        print("cac canh cua da giac la: ")
     }
 }
  
@@ -83,7 +80,6 @@ class TamGiac : DaGiac {
     func pyTaGo() -> String {
         return pow(Decimal(a[0]), 2) + pow(Decimal(a[1]), 2) == pow(Decimal(a[2]), 2) ? "thoả mãn điều kiện, các cạnh của tam giác là : \(a[0]), \(a[1]), \(a[2])" : "không thoả"
     }
-    
 }
 
 let tamGiac1 = TamGiac(soCanh: 3, a: [3,4,5])
@@ -112,29 +108,28 @@ struct CStack {
         return top == -1
     }
     
-    mutating func pUSH( value: Int, capacity: Int) {             //thêm một phần tử vào stack
+    mutating func push( value: Int, capacity: Int) {             //thêm một phần tử vào stack
     if(isFull(stack: capacity) == true) {
-        print("\nStack is full. Overflow condition!");
+        print("Stack is full. Overflow condition!");
     } else {
-        top+=1
+        top += 1
         array[top] = value
-        //stack[top] = value;
     }
     }
     
-    mutating func pOP() {                                                     //xoá 1 phần tử ở đỉnh stack
+    mutating func pop() {                                                     //xoá 1 phần tử ở đỉnh stack
     if(isEmpty() == true) {
         print("\nStack is empty. Underflow condition!");
     } else {
-        top-=1
+        top -= 1
     }
     }
 }
 var stack = CStack(top: -1, capacity: 3, array: [1,2,3])
 stack.isEmpty()
-stack.pUSH(value: 10, capacity: 3)
+stack.push(value: 10, capacity: 3)
 stack.isEmpty()
-stack.pOP()
+stack.pop()
 
 //Bài 06----------------------------------------------------------------------------------------------------------------
 class HinhVe {
@@ -166,18 +161,15 @@ class Tron {
     }
     
     func tinhChuViHinhTron() -> Float {
-        let pi: Float = 3.14
-        return r * 2 * pi
+        return r * 2 * Float.pi
     }
     
     func tinhDienTichHinhTron() -> Float {
-        let pi: Float = 3.14
-        return pi * pow(r, 2)
+        return Float.pi * pow(r, 2)
     }
     
     func tinhTheTichHinhTron() -> Float {
-        let pi: Float = 3.14
-        return pi * 2 * r
+        return Float.pi * 2 * r
     }
 }
 
@@ -244,7 +236,6 @@ class LapPhuong : BaChieu {
     func tinhTheTichHinhLapPhuong() -> Float {
         return pow(Float(soCanh), 3)
     }
-    
 }
 
 class Cau {
@@ -255,13 +246,12 @@ class Cau {
     }
     
     func tinhTheTichCau() -> Float {
-        let pi: Float = 3.14
-        return 4/3 * pi * 3 * r
+        return 4/3 * Float.pi * 3 * r
     }
 }
-let hinhVuong = Vuong(soCanh: 4, array: [3,3,3,3])
+let hinhVuong = Vuong(soCanh: 4, array: [3, 3, 3, 3])
 let tamGiac = Tamg(soCanh: 3, array: [3,4,5])
-let hinhLapPhuong = LapPhuong(soCanh: 12, array: [4,4,4,4,4,4,4,4,4,4,4,4])
+let hinhLapPhuong = LapPhuong(soCanh: 12, array: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4])
 print("chu vi của hình vuông là: \(hinhVuong.tinhChuVi())")
 print("diện tích của tam giác là: \(tamGiac.tinhDienTich())")
 print("diện tích hình lập phương là: \(hinhLapPhuong.tinhDienTichHinhLapPhuong())")
@@ -305,7 +295,7 @@ class DSHocSinh {
     func inKetQua(ds: [HocSinh]) -> String {
         var kq = ""
         for hs in ds {
-            kq += hs.hoTen + "    \(hs.tongDiem)" + "\n"
+            kq += hs.hoTen + "\(hs.tongDiem)" + "\n"
         }
         return kq
     }
@@ -315,7 +305,6 @@ var hs2 = HocSinh(hoTen: "nguyen van mmka", namSinh: 1999, tongDiem: 9.0)
 var hs3 = HocSinh(hoTen: "nguyen van opop", namSinh: 1999, tongDiem: 7.0)
 var hs4 = HocSinh(hoTen: "nguyen van b", namSinh: 1999, tongDiem: 10.0)
 var hs5 = HocSinh(hoTen: "nguyen van alll", namSinh: 1997, tongDiem: 10.0)
-
 var ds = DSHocSinh(ds: [hs1, hs2, hs3, hs4, hs5])
 print(ds.inKetQua(ds: ds.sapXep()))
 
@@ -324,7 +313,7 @@ class Date {
     var Day: Int
     var Month: Int
     var Year: Int
-    var Days: [Int] = [31,28,31,30,31,30,31,31,30,31,30,31]
+    var Days: [Int] = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     
     init(Day: Int, Month: Int, Year: Int) {
         self.Day = Day
@@ -333,44 +322,42 @@ class Date {
     }
     
     func normalize(DD: Int, MM: Int, YY: Int) {
-        var Month = (MM>=1 && MM<=12) ? MM : 1
-        var Year = (YY>=1900 && YY<=2100) ? YY : 1900
-        if(Month==2 && ktrNhuan(Y: Year)) {
-            var Day = (DD>=1&&DD<=29) ? DD : 1
-        }
-        else {
-            var Day = (DD>=1&&DD<=Days[Month-1]) ? DD : 1
+        var Month = MM >= 1 && MM <= 12 ? MM : 1
+        var Year = YY >= 1900 && YY <= 2100 ? YY : 1900
+        if Month == 2 && ktrNhuan(Y: Year) {
+            var Day = DD >= 1 && DD <= 29 ? DD : 1
+        } else {
+            var Day = DD >= 1 && DD <= Days[Month-1] ? DD : 1
         }
     }
     
     func advance() {
-        if( Day == daysIn(D: Day)) {      // Het Nam
-            Day=1
-            Month=1
-            Year+=1
-        } else if((daysIn(D: Day)) != 0) {          //Het Thang
+        if Day == daysIn(D: Day) {      // Het Nam
             Day = 1
-            Month+=1
+            Month = 1
+            Year += 1
+        } else if daysIn(D: Day) != 0 {          //Het Thang
+            Day = 1
+            Month += 1
         } else {
             Day+=1
         }
     }
     
     func daysIn(D: Int) -> Int {
-        if(Month == 2 && ktrNhuan(Y: Year)) {
+        if Month == 2 && ktrNhuan(Y: Year) {
             return 29
         }
-            return Days[Month - 1]
+        return Days[Month - 1]
     }
     
     func ktrNhuan(Y: Int) -> Bool {
-        if (Y % 4 == 0 && Y % 100 != 0) {
+        if Y % 4 == 0 && Y % 100 != 0 {
+            return true
+        } else if Y % 400 == 0 {
             return true
         }
-        if (Y % 400 == 0) {
-            return true
-        }
-            return false
+        return false
     }
 }
 let ngay01 = Date(Day: 1, Month: 1, Year: 1900)
@@ -378,7 +365,7 @@ ngay01.ktrNhuan(Y: 1988)
 ngay01.normalize(DD: 12, MM: 5, YY: 1985)
 
 //Bài 09----------------------------------------------------------------------------------------------------------------
-struct Mang1c {
+struct Mang {
     var n: Int
     var a:[Int]
     
@@ -393,7 +380,7 @@ struct Mang1c {
         }
     }
     
-    func ptuLonNhatMang() -> Int {
+    func maxMang() -> Int {
         var max: Int = 0
         for i in 0..<n {
             if (a[i] > max) {
@@ -403,7 +390,7 @@ struct Mang1c {
         return max
     }
     
-    func ptuNhoNhatMang() -> Int {
+    func minMang() -> Int {
         var min: Int = a[0]
         for i in 1..<n {
             if (a[i] < min) {
@@ -413,29 +400,29 @@ struct Mang1c {
         return min
     }
 }
-let a = Mang1c(n: 3, a: [10,20,30])
+let a = Mang(n: 3, a: [10, 20, 30])
 a.xuatMang()
-print("phần tử lớn nhất trong mảng là: \(a.ptuLonNhatMang())")
-print("phần tử nhỏ nhất trong mảng là: \(a.ptuNhoNhatMang())")
+print("phần tử lớn nhất trong mảng là: \(a.maxMang())")
+print("phần tử nhỏ nhất trong mảng là: \(a.minMang())")
 
 //Bài 10----------------------------------------------------------------------------------------------------------------
 struct A {
-    var S: Int
+    var s: Int
     var n: Int
     
-    init(S: Int, n: Int) {
-        self.S = S
+    init(s: Int, n: Int) {
+        self.s = s
         self.n = n
     }
     
     mutating func tinhTong() -> Int {
         for i in 1...n {
-            S+=i
+            s += i
         }
-        return S
+        return s
     }
 }
-var nhapn = A(S: 0, n: 10)
+var nhapn = A(s: 0, n: 10)
 print("tổng số vừa nhập là: \(nhapn.tinhTong())")
 
 
