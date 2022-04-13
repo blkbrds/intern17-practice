3bff1286ffcc1b912fc211a760203257d4a85535
Phong Huynh N <phong.huynh@monstar-lab.com>
2022-04-13 15:34:18 +0700

    Exercise Autolayout

--- 07_Autolayout/PhongHuynh/Auto Layout/Auto Layout/Bai09/Bai9ViewController.swift
+++ 07_Autolayout/PhongHuynh/Auto Layout/Auto Layout/Bai09/Bai9ViewController.swift
@@ -21,29 +21,24 @@ final class Bai9ViewController: UIViewController {
 
     // MARK: - IBActions
     @IBAction private func rightTouchUpInsinde(_ sender: Any) {
-        for index in 0...array.count {
-                nameLabel.text = array[index].message
-                imageView.image = UIImage(named: array[index].image)
-                textLabel.text = "2/5"
-                
+        if catScrollView.contentOffset.x < self.view.bounds.width * CGFloat(array.count-2) {
+            catScrollView.contentOffset.x +=  self.view.bounds.width
+            index += 1
+            imageView.image = UIImage(named: array[index].image)
+            nameLabel.text = array[index].message
+            textLabel.text = "\(index + 1)/5"
             
         }
     }
     
     @IBAction func leftTouchUpInside(_ sender: Any) {
-        for index in 0..<array.count {
-            if index > 0 {
-                imageView.image = UIImage(named: array[index].image)
-                nameLabel.text = array[index].message
-            }
+        if catScrollView.contentOffset.x > 0 {
+            catScrollView.contentOffset.x -=  self.view.bounds.width
+            imageView.image = UIImage(named: array[index - 1].image)
+            nameLabel.text = array[index - 1].message
         }
     }
     
-    
-
-    
-    
-
 }
 
 class Meo {
@@ -59,6 +54,6 @@ class Meo {
 let array: [Meo] = [Meo(image: "meo1.jpeg", message: "Cắt, phóng to hoặc thu nhỏ ảnh sẽ không còn là nỗi ám ảnh của bạn nữa vì bài viết này mình sẽ hướng dẫn các bạn cách thực hiện điều đó. Có nhiều cách để resize ảnh nhưng hôm nay mình sẽ hướng dẫn các bạn những cách đơn giản và nhanh chóng nhất trêCắt, phóng to hoặc thu nhỏ ảnh sẽ không còn là nỗi ám ảnh của bạn nữa vì bài viết này mình sẽ hướng dẫn các bạn cách thực hiện điều đó. Có nhiều cách để resize ảnh nhưng hôm nay mình sẽ hướng dẫn các bạn những cách đơn giản và nhanh chóng nhất trê"),
                     Meo(image: "meo2.jpeg", message: "Cắt, phóng to hoặc thu nhỏ ảnh sẽ không còn là nỗi ám ảnh của bạn nữa vì bài viết này mình sẽ hướng dẫn các bạn cách thực hiện điều đó. Có nhiều cách để resize ảnh nhưng hôm nay mình sẽ hướng dẫn các bạn những cách đơn giản và nhanh chóng nhất trê"),
                     Meo(image: "meo3.jpeg", message: "Cắt, phóng to hoặc thu nhỏ ảnh sẽ không còn là nỗi ám ảnh của bạn nữa vì bài viết này mình sẽ hướng dẫn các bạn cách thực hiện điều đó. Có nhiều cách để resize ảnh nhưng hôm nay mình sẽ hướng dẫn các bạn những cách đơn giản và nhanh chóng nhất trêCắt, phóng to hoặc thu nhỏ ảnh sẽ không còn là nỗi ám ảnh của bạn nữa vì bài viết này mình sẽ hướng dẫn các bạn cách thực hiện điều đó. Có nhiều cách để resize ảnh nhưng hôm nay mình sẽ hướng dẫn các bạn những cách đơn giản và nhanh chóng nhất trêCắt, phóng to hoặc thu nhỏ ảnh sẽ không còn là nỗi ám ảnh của bạn nữa vì bài viết này mình sẽ hướng dẫn các bạn cách thực hiện điều đó. Có nhiều cách để resize ảnh nhưng hôm nay mình sẽ hướng dẫn các bạn những cách đơn giản và nhanh chóng nhất trê"),
-                    Meo(image: "meo4.jpeg", message: "Cắt, phóng to hoặc thu nhỏ ảnh sẽ không còn là nỗi ám ảnh của bạn nữa vì bài viết này mình sẽ hướng dẫn các bạn cách thực hiện điều đó. Có nhiều cách để resize ảnh nhưng hôm nay mình sẽ hướng dẫn các bạn những cách đơn giản và nhanh chóng nhất trê"),
-                    Meo(image: "meo5.jpeg", message: "Cắt, phóng to hoặc thu nhỏ ảnh sẽ không còn là nỗi ám ảnh của bạn nữa vì bài viết này mình sẽ hướng dẫn các bạn cách thực hiện điều đó. Có nhiều cách để resize"),]
+                    Meo(image: "meo4.jpeg", message: "Cắt, phóng to hoặc thu nhỏ ảnh sẽ không còn là nỗi ám ảnh của bạn nữa vì bài viết này mình sẽ hướng dẫn các bạn cách thực hiện điều đó. Có nhiều cách để resize ảnh nhưng hôm nanh sẽ không còn là nỗi ám ảnh của bạn nữa vì bài viết này mình sẽ hướng dẫn các bạn cách thực hiện điều đó. Có nhiều cách để resize ảnh nhưng hôm nay mình sẽ hướng dẫn các bạn những cách đơn giản và nhanh chóng nhất trêCắt, phóng to hoặc thu nhỏ ảnh sẽ không còn là nỗi ám ảnh của bạn nữa vì bài viết này mình sẽ hướng dẫn các bạn cách thực hiện điều đó. Có nhiều cách để resize ảnh nhưng hôm nay mình sẽ hướng dẫn các bạn những cách đơn giản và nhanh chóng nhất trêy mình sẽ hướng dẫn các bạn những cách đơn giản và nhanh chóng nhất trê"),
+                    Meo(image: "meo5.jpeg", message: "Cắt, phóng to hoặc thu nhỏ ảnh sẽ không còn là nỗi ám ảnh của bạn nữa vì bài viết này mình sẽ hướng dẫn các bạn cách thực hiện điều đó. Có nhiều cnh sẽ không còn là nỗi ám ảnh của bạn nữa vì bài viết này mình sẽ hướng dẫn các bạn cách thực hiện điều đó. Có nhiều cách để resize ảnh nhưng hôm nay mình sẽ hướng dẫn các bạn những cách đơn giản và nhanh chóng nhất trêCắt, phóng to hoặc thu nhỏ ảnh sẽ không còn là nỗi ám ảnh của bạn nữa vì bài viết này mình sẽ hướng dẫn các bạn cách thực hiện điều đó. Có nhiều cách để resize ảnh nhưng hôm nay mình sẽ hướng dẫn các bạn những cách đơn giản và nhanh chóng nhất trênh sẽ không còn là nỗi ám ảnh của bạn nữa vì bài viết này mình sẽ hướng dẫn các bạn cách thực hiện điều đó. Có nhiều cách để resize ảnh nhưng hôm nay mình sẽ hướng dẫn các bạn những cách đơn giản và nhanh chóng nhất trêCắt, phóng to hoặc thu nhỏ ảnh sẽ không còn là nỗi ám ảnh của bạn nữa vì bài viết này mình sẽ hướng dẫn các bạn cách thực hiện điều đó. Có nhiều cách để resize ảnh nhưng hôm nay mình sẽ hướng dẫn các bạn những cách đơn giản và nhanh chóng nhất trêách để resize"),]
 
---
+++ 07_Autolayout/PhongHuynh/Auto Layout/Auto Layout/Bai14/Bai14ViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class Bai14ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+
+}
