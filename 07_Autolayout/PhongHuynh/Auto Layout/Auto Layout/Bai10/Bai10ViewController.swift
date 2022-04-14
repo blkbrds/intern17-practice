0af553912f59841800768362ae53c038c95e99c1
Phong Huynh N <phong.huynh@monstar-lab.com>
2022-04-14 17:19:17 +0700

    Edit Autolayout

--- 07_Autolayout/PhongHuynh/Auto Layout/Auto Layout/Bai10/Bai10ViewController.swift
+++ 07_Autolayout/PhongHuynh/Auto Layout/Auto Layout/Bai10/Bai10ViewController.swift
@@ -2,21 +2,96 @@ import UIKit
 
 final class Bai10ViewController: UIViewController, UIScrollViewDelegate {
 
-  
-    @IBOutlet weak var view1: UIView!
-    @IBOutlet weak var view2: UIView!
-    
+    @IBOutlet private weak var view1: UIView!
+    @IBOutlet private weak var view2: UIView!
+    @IBOutlet private weak var view3: UIView!
+    @IBOutlet private weak var view4: UIView!
 
-    
+    @IBOutlet private weak var greenViewConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var pupleViewConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var yellowViewConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var redViewConstraint: NSLayoutConstraint!
+
+    @IBOutlet private weak var greenViewScale: NSLayoutConstraint!
+    @IBOutlet private weak var pupleViewScale: NSLayoutConstraint!
+    @IBOutlet private weak var yellowViewScale: NSLayoutConstraint!
+    @IBOutlet private weak var redViewScale: NSLayoutConstraint!
+
+    let width: CGFloat = 220
     override func viewDidLoad() {
         super.viewDidLoad()
-   
     }
 
-    @IBAction func zoomView(_ sender: Any) {
-        let h: CGFloat = view2.bounds.size.height
-        let w: CGFloat = view2.bounds.size.width
-        view2.frame = CGRect(x: view2.frame.origin.x, y: view2.frame.origin.y, width: w * 2, height: h * 2)
+    @IBAction private func clickGreenViewTouchUpInside(_ sender: Any) {
+        greenViewScale.priority = UILayoutPriority(rawValue: 250)
+        greenViewConstraint.priority = UILayoutPriority(rawValue: 750)
+        greenViewConstraint.constant = width + (width / 3)
+
+        pupleViewScale.priority = UILayoutPriority(rawValue: 250)
+        pupleViewConstraint.priority = UILayoutPriority(rawValue: 750)
+        pupleViewConstraint.constant = width - (width / 3)
+
+        yellowViewScale.priority = UILayoutPriority(rawValue: 250)
+        yellowViewConstraint.priority = UILayoutPriority(rawValue: 750)
+        yellowViewConstraint.constant = width - (width / 3)
+
+        redViewScale.priority = UILayoutPriority(rawValue: 250)
+        redViewConstraint.priority = UILayoutPriority(rawValue: 750)
+        redViewConstraint.constant = width - (width / 3)
+    }
+
+    @IBAction private func clickPupleViewTouchUpInside(_ sender: Any) {
+        greenViewScale.priority = UILayoutPriority(rawValue: 250)
+        greenViewConstraint.priority = UILayoutPriority(rawValue: 750)
+        greenViewConstraint.constant = width - (width / 3)
+
+        pupleViewScale.priority = UILayoutPriority(rawValue: 250)
+        pupleViewConstraint.priority = UILayoutPriority(rawValue: 750)
+        pupleViewConstraint.constant = width + (width / 3)
+
+        yellowViewScale.priority = UILayoutPriority(rawValue: 250)
+        yellowViewConstraint.priority = UILayoutPriority(rawValue: 750)
+        yellowViewConstraint.constant = width - (width / 3)
+
+        redViewScale.priority = UILayoutPriority(rawValue: 250)
+        redViewConstraint.priority = UILayoutPriority(rawValue: 750)
+        redViewConstraint.constant = width - (width / 3)
     }
     
+    @IBAction private func clickYellowViewTouchUpInside(_ sender: Any) {
+        greenViewScale.priority = UILayoutPriority(rawValue: 250)
+        greenViewConstraint.priority = UILayoutPriority(rawValue: 750)
+        greenViewConstraint.constant = width - (width / 3)
+
+        pupleViewScale.priority = UILayoutPriority(rawValue: 250)
+        pupleViewConstraint.priority = UILayoutPriority(rawValue: 750)
+        pupleViewConstraint.constant = width - (width / 3)
+
+        yellowViewScale.priority = UILayoutPriority(rawValue: 250)
+        yellowViewConstraint.priority = UILayoutPriority(rawValue: 750)
+        yellowViewConstraint.constant = width + (width / 3)
+
+        redViewScale.priority = UILayoutPriority(rawValue: 250)
+        redViewConstraint.priority = UILayoutPriority(rawValue: 750)
+        redViewConstraint.constant = width - (width / 3)
+    }
+
+    
+    @IBAction private func clickRedViewTouchUpInside(_ sender: Any) {
+        greenViewScale.priority = UILayoutPriority(rawValue: 250)
+        greenViewConstraint.priority = UILayoutPriority(rawValue: 750)
+        greenViewConstraint.constant = width - (width / 3)
+
+        pupleViewScale.priority = UILayoutPriority(rawValue: 250)
+        pupleViewConstraint.priority = UILayoutPriority(rawValue: 750)
+        pupleViewConstraint.constant = width - (width / 3)
+
+        yellowViewScale.priority = UILayoutPriority(rawValue: 250)
+        yellowViewConstraint.priority = UILayoutPriority(rawValue: 750)
+        yellowViewConstraint.constant = width - (width / 3)
+
+        redViewScale.priority = UILayoutPriority(rawValue: 250)
+        redViewConstraint.priority = UILayoutPriority(rawValue: 750)
+        redViewConstraint.constant = width + (width / 3)
+    }
 }
