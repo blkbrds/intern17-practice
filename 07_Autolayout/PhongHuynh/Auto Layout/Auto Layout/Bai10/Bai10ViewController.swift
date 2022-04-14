09f62048272da715d0af6285733244a9c81f71a4
Phong Huynh N <phong.huynh@monstar-lab.com>
2022-04-14 12:54:52 +0700

    Update autolayout

--- 07_Autolayout/PhongHuynh/Auto Layout/Auto Layout/Bai10/Bai10ViewController.swift
+++ 07_Autolayout/PhongHuynh/Auto Layout/Auto Layout/Bai10/Bai10ViewController.swift
@@ -2,9 +2,11 @@ import UIKit
 
 final class Bai10ViewController: UIViewController, UIScrollViewDelegate {
 
-    @IBOutlet private weak var view1: UIView!
-    @IBOutlet private weak var view2: UIView!
-    @IBOutlet private weak var view4: UIView!
+  
+    @IBOutlet weak var view1: UIView!
+    @IBOutlet weak var view2: UIView!
+    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -12,7 +14,9 @@ final class Bai10ViewController: UIViewController, UIScrollViewDelegate {
     }
 
     @IBAction func zoomView(_ sender: Any) {
-        
+        let h: CGFloat = view2.bounds.size.height
+        let w: CGFloat = view2.bounds.size.width
+        view2.frame = CGRect(x: view2.frame.origin.x, y: view2.frame.origin.y, width: w * 2, height: h * 2)
     }
     
 }
