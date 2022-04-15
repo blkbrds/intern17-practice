2d25f3bbb303f552678a24c6c6250acf33214e91
Phong Huynh N <phong.huynh@monstar-lab.com>
2022-04-15 17:46:34 +0700

    Exercise MVC

--- 08_MVC/PhongHuynh/MVC/MVC/Models/Calculator.swift
+++ 08_MVC/PhongHuynh/MVC/MVC/Models/Calculator.swift
@@ -1,7 +1,18 @@
 import UIKit
 
-class Calculator: UIView {
-
-
+class Calculator {
+    
+    var numberOnScreen: Double
+    var previousNumber: Double
+    
+    init(numberOnScreen: Double, previousNumber: Double) {
+        self.numberOnScreen = numberOnScreen
+        self.previousNumber = previousNumber
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 
 }
