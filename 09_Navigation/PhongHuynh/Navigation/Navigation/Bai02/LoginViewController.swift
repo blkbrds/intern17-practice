d648c89de661e93ad51e3c1ff45d6ab692d7bf3c
Phong Huynh N <phong.huynh@monstar-lab.com>
2022-04-17 15:40:38 +0700

    Exercise2 Navigation

--- 09_Navigation/PhongHuynh/Navigation/Navigation/Bai02/LoginViewController.swift
+++ 09_Navigation/PhongHuynh/Navigation/Navigation/Bai02/LoginViewController.swift
@@ -4,7 +4,6 @@ enum Check: String {
     case noUserName = "Chưa nhập tên đăng nhập"
     case noPassword = "Chưa nhập mật khẩu"
     case empty = "Chưa nhập dữ liệu"
-    case correct = "Nhập đúng"
     case incorrect = "Nhập sai tên đặng nhập hoặc mật khẩu"
 }
 
@@ -13,26 +12,49 @@ final class LoginViewController: UIViewController {
     // MARK: - IBOutlets
     @IBOutlet private weak var usernameTextField: UITextField!
     @IBOutlet private weak var passwordTextField: UITextField!
+    @IBOutlet weak var showLabel: UILabel!
     
-    var check: Check?
+    var check: Check = .empty {
+        didSet {
+            showResult()
+        }
+    }
     
     // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         title = "Login"
         let rightButton = UIBarButtonItem(title: "Don", style: .plain, target: self, action: #selector(rightAction))
         navigationItem.rightBarButtonItem = rightButton
         
+        usernameTextField.delegate = self
+        passwordTextField.delegate = self
+        passwordTextField.returnKeyType = .done
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+           usernameTextField.resignFirstResponder()
+           passwordTextField.resignFirstResponder()
     }
     
     @objc func rightAction() {
-        let vc = HomeViewController()
-        self.navigationController?.pushViewController(vc, animated: true)
         guard let userName = usernameTextField.text, let password = passwordTextField.text else { return }
         let user = User(userName: userName, password: password)
-        if check == user.checkUserPassword() {
+        check = user.checkUserPassword()
+        login()
+    }
+    
+    func login() -> Bool {
+        let vc = HomeViewController()
+        if usernameTextField.text == "username" && passwordTextField.text == "password"  {
+            return ((self.navigationController?.pushViewController(vc, animated: true)) != nil)
         }
+        return false
+    }
+    
+    func showResult() {
+        showLabel.text = check.rawValue
     }
 
 }
@@ -43,13 +65,22 @@ struct User {
     
     func checkUserPassword() -> Check {
         switch (userName, password) {
-        case ("abc", "123"): return .correct
         case ("", ""): return .empty
         case (_, ""): return .noPassword
         case ("", _): return .noUserName
         default: return .incorrect
         }
     }
+}
+
+extension LoginViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            passwordTextField.becomeFirstResponder()
+            if textField.returnKeyType == .done {
+
+            }
+            return true
+        }
 }
 
     
