d3e47c1d0a2b9c7d902de3f0ee0014a689d3632d
Phong Huynh N <phong.huynh@monstar-lab.com>
2022-04-19 16:10:58 +0700

    Exercise1 Protocol

---
+++ 10_Protocol/PhongHuynh/Protocol/Protocol/Bai01/Bai01ViewController.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+class Thu {
+    var isDi: Bool
+    var ten: [String]
+    
+    init(isDi: Bool, ten: [String]) {
+        self.isDi = isDi
+        self.ten = ten
+    }
+}
+
+class Chim {
+    var isBay: Bool
+    var ten: [String]
+    
+    init(isBay: Bool, ten: [String]) {
+        self.isBay = isBay
+        self.ten = ten
+    }
+}
+
+class Ca {
+    var isBoi: Bool
+    var ten: [String]
+    
+    init(isBoi: Bool, ten: [String]) {
+        self.isBoi = isBoi
+        self.ten = ten
+    }
+}
+
+class Bai01ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
