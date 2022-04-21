4aef3ac193efb34c1f39f8f28d2acb34e530a41b
Tuan Tran V <tuan.tran@monstar-lab.com>
2022-04-21 15:16:49 +0700

    finish BT-OOP

--- 02_SwiftBasic/TuanTV/BT-OOP-BT2.playground/Contents.swift
+++ 02_SwiftBasic/TuanTV/BT-OOP-BT2.playground/Contents.swift
@@ -85,8 +85,131 @@ class CStack {
             stack[top] = 0
         }
     }
-    
-    
+}
 
+//MARK: -BAI TAP 6
+class HinhVe {
+    func tinhChuVi() -> Float {
+        return 1.0
+    }
+    func tinhDienTich() -> Float {
+        return 1.0
+    }
+}
+class HaiChieu: HinhVe {
     
 }
+class HinhTron: HaiChieu {
+    var banKinh: Float
+    let pi: Float = 3.14
+    init(banKinh: Float) {
+        self.banKinh = banKinh
+    }
+    override func tinhChuVi() -> Float {
+        return 2 * banKinh * pi
+    }
+    override func tinhDienTich() -> Float {
+        return pi * banKinh * banKinh
+    }
+}
+class HinhVuong: HaiChieu {
+    var canh: Float
+    init(canh: Float) {
+        self.canh = canh
+    }
+    override func tinhChuVi() -> Float {
+        return 4 * canh
+    }
+    override func tinhDienTich() -> Float {
+        return canh * canh
+    }
+}
+class TamGiac1: HaiChieu {
+    var canhA: Float
+    var canhB: Float
+    var canhC: Float
+    init(canhA: Float, canhB: Float, canhC: Float) {
+        self.canhA = canhA
+        self.canhB = canhB
+        self.canhC = canhC
+    }
+    override func tinhChuVi() -> Float {
+        return canhA + canhB + canhC
+    }
+    override func tinhDienTich() -> Float {
+        var chuVi = tinhChuVi()
+        return sqrtf(chuVi * (chuVi - canhA) * (chuVi - canhB) * (chuVi - canhC))
+    }
+}
+class BaChieu: HinhVe {
+    func tinhTheTich() -> Float {
+        return 1.0
+    }
+}
+class HinhCau: BaChieu {
+    var banKinh: Float
+    let pi: Float = 3.14
+    init(banKinh: Float) {
+        self.banKinh = banKinh
+    }
+    override func tinhChuVi() -> Float {
+        return 2 * pi * banKinh
+    }
+    override func tinhDienTich() -> Float {
+        return 4 * pi * banKinh * banKinh
+    }
+    override func tinhTheTich() -> Float {
+        return 4 / 3 * pi * banKinh * banKinh * banKinh
+    }
+}
+class HinhlapPhuong: BaChieu {
+    var canh: Float
+    init(canh: Float) {
+        self.canh = canh
+    }
+    override func tinhChuVi() -> Float {
+        return 12 * canh
+    }
+    override func tinhTheTich() -> Float {
+        return 4 * canh * canh
+    }
+    override func tinhDienTich() -> Float {
+        return canh * canh * canh
+    }
+    
+}
+
+//MARK: -BAI TAP 7
+class HocSinh {
+    var ho: String
+    var ten: String
+    var namSinh: Int
+    var tongDiem: Int
+    init(ho: String, ten: String, namSinh: Int, tongDiem: Int) {
+        self.ho = ho
+        self.ten = ten
+        self.namSinh = namSinh
+        self.tongDiem = tongDiem
+    }
+    func xepLoaiHocSinh(dsHocSinh: [HocSinh]) {
+        var dsHocSinhTemp: [HocSinh] = [HocSinh]()
+        dsHocSinhTemp = dsHocSinh.sorted(by: {
+            if $0.tongDiem == $1.tongDiem {
+                return $0.namSinh < $1.namSinh
+            } else {
+                return $0.tongDiem > $1.tongDiem
+            }
+        })
+        for ds in dsHocSinhTemp{
+            print(ds.namSinh)
+        }
+        
+    }
+    func testBT7() {
+        xepLoaiHocSinh(dsHocSinh: [HocSinh(ho: "Tuan", ten: "Tran", namSinh: 2015, tongDiem: 1), HocSinh(ho: "van", ten: "tran",namSinh: 2014, tongDiem: 2), HocSinh(ho: "Tran", ten: "Di", namSinh: 2013, tongDiem: 2), HocSinh(ho: "THien", ten: "Di", namSinh: 2012, tongDiem: 4)])
+        print()
+    }
+    
+}
+HocSinh(ho: "a", ten: "b", namSinh: 2011, tongDiem: 2).testBT7()
+
