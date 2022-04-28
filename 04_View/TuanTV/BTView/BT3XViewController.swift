0d70dcdbf9c8d9277ccc3f3e0ead31b1bed75377
Tuan Tran V <tuan.tran@monstar-lab.com>
2022-04-28 11:58:43 +0700

    Done BT3X

---
+++ 04_View/TuanTV/BTView/BT3XViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BT3-6ViewController.swift
+//  BTView
+//
+//  Created by Tuan Tran V. VN.Danang on 4/28/22.
+//
+
+import UIKit
+
+class BT3_6ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
