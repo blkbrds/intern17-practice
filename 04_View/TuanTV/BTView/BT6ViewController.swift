de81982cc35eb23e2a856c2d98c529bd6e75863a
Tuan Tran V <tuan.tran@monstar-lab.com>
2022-04-29 14:06:13 +0700

    Done Bải Tập 7

--- 04_View/TuanTV/BTView/BT6ViewController.swift
+++ 04_View/TuanTV/BTView/BT6ViewController.swift
@@ -9,8 +9,15 @@ import UIKit
 
 class BT6ViewController: UIViewController {
     let imageView = UIImageView()
+    let imvChat = UIImageView()
+    let lbChat = UILabel()
     var pinchGesture = UIPinchGestureRecognizer()
     var rotationGesture = UIRotationGestureRecognizer()
+    var longPressGesture = UILongPressGestureRecognizer()
+    var tapGesture = UITapGestureRecognizer()
+    var maxScale:CGFloat = 2.0
+    var minScale:CGFloat = 0.5
+    var tapCount = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +32,20 @@ class BT6ViewController: UIViewController {
         imageView.frame = CGRect(x: widthView / 2 - 50, y: heightView / 2 - 50, width: 100, height: 100)
         imageView.image = UIImage(named: "AnimalImage")
         view.addSubview(imageView)
+        
+        imvChat.frame = CGRect(x: 50, y: 50, width: 200, height: 200)
+        imvChat.image = UIImage(named: "ChatFrame")
+        imvChat.backgroundColor = .red
+        imvChat.contentMode = .scaleToFill
+        imvChat.isHidden = true
+        imvChat.addSubview(lbChat)
+        view.addSubview(imvChat)
+        
+        lbChat.frame = CGRect(x: 0, y: 80, width: 200, height: 40)
+        lbChat.text = ""
+        lbChat.textAlignment = .center
+//        lbChat.isHidden = true
+        lbChat.backgroundColor = .clear
     }
     
     func addGesture() {
@@ -35,15 +56,60 @@ class BT6ViewController: UIViewController {
         rotationGesture = UIRotationGestureRecognizer(target: self, action: #selector(self.handleRotationGesture(rotationGesture:)))
         imageView.addGestureRecognizer(rotationGesture)
         
-        let longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPressGesture(longPressGesture:)))
+        longPressGesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPressGesture(longPressGesture:)))
         longPressGesture.minimumPressDuration = 5
         imageView.addGestureRecognizer(longPressGesture)
+        
+        tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTapGesture(tapGesture:)))
+        tapGesture.numberOfTapsRequired = 1
+        imageView.addGestureRecognizer(tapGesture)
+        
+        
         
     }
     
     var recognizerScale:CGFloat = 1.0
-    var maxScale:CGFloat = 2.0
-    var minScale:CGFloat = 0.5
+    
+    
+    @objc func handleTapGesture(tapGesture: UITapGestureRecognizer){
+        print("tap")
+        guard tapGesture.view != nil else { return }
+            tapCount += 1
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.handleTapCount(count: self.tapCount)
+        }
+    }
+    
+    private func handleTapCount(count: Int) {
+        print("handleTap")
+        tapCount = 0
+        switch count {
+        case 1:
+            imvChat.isHidden = false
+//            lbChat.isHidden = false
+            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+//                self.imvChat.alpha = 1.0
+                UIView.animate(withDuration: 0.5) {
+//                    self.imvChat.alpha = 0
+                    self.imvChat.isHidden = true
+                    
+                }
+            }
+            lbChat.text = "Toi la khi"
+        case 2:
+            imvChat.isHidden = false
+            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+//                self.imvChat.alpha = 1.0
+                UIView.animate(withDuration: 0.5) {
+//                    self.imvChat.alpha = 0
+                    self.imvChat.isHidden = true
+                }
+            }
+            lbChat.text = "Khi la toi"
+        default:
+            break
+        }
+    }
     
     @objc func handleLongPressGesture(longPressGesture: UILongPressGestureRecognizer){
         guard longPressGesture.view != nil else { return }
