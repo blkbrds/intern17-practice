20f99ec73a157ba360464f14887f2ff8761504f9
Tinh Bui T <tinh.bui@monstar-lab.com>
2022-05-05 16:27:48 +0700

    Fix Error exercise swift basic

--- 02_SwiftBasic/TinhBT/MyPlayground.playground/Contents.swift
+++ 02_SwiftBasic/TinhBT/MyPlayground.playground/Contents.swift
@@ -2,7 +2,8 @@ import Foundation
 
 var str = "Hello, playground"
 
-func tinhDienTich(banKinh: Double) -> (dienTich: Double, theTich: Double) {
+func tinhDienTichVaTheTich(banKinh: Double) -> (dienTich: Double, theTich: Double) {
+    
     var dienTich: Double = 0
     var theTich: Double = 0
     dienTich = Double(4 / 3 * 3.14 * banKinh * banKinh * banKinh)
@@ -10,16 +11,17 @@ func tinhDienTich(banKinh: Double) -> (dienTich: Double, theTich: Double) {
     return (dienTich, theTich)
 }
 
-tinhDienTich(banKinh: 5)
+print("dien tich là: \(tinhDienTichVaTheTich(banKinh: 5).dienTich), the tich là: \(tinhDienTichVaTheTich(banKinh: 5).theTich)")
 
 //bai2
+
 func tinhPhuongTrinhBac2(a: Double, b: Double, c: Double) -> (x1: Double, x2: Double) {
+    
     var x1: Double = 0
     var x2: Double = 0
-    
     var detar: Double = 0
     detar = b * b - (4 * a * c)
-    if     detar < 0 {
+    if detar < 0 {
         print("phuong trinh vo nghiem")
     } else if detar == 0 {
         let nghiemKep = -(b / 2 * a)
@@ -32,10 +34,12 @@ func tinhPhuongTrinhBac2(a: Double, b: Double, c: Double) -> (x1: Double, x2: Do
     return (x1, x2)
 }
 
-tinhPhuongTrinhBac2(a: 2, b: 4, c: 2)
+print("phuong trinh bac hai la: \(tinhPhuongTrinhBac2(a: 2, b: 4, c: 2))")
 
 //bai 3
+
 func tinhPhuongTrinhBac1(a: Double, b: Double, c:Double, a1: Double, b1: Double, c1: Double) -> (x: Double, y: Double) {
+    
     var x: Double = 0
     var y: Double = 0
     var dx: Double = 0
@@ -44,14 +48,15 @@ func tinhPhuongTrinhBac1(a: Double, b: Double, c:Double, a1: Double, b1: Double,
     d = a * b1 - a1 * b
     dx = c * b1 - c1 * b
     dy = a * c1 - a1 * c
-    
     x = dx / d
     y = dy / d
     return (x, y)
 }
-tinhPhuongTrinhBac1(a: 2, b: 3, c: 4, a1: 3, b1: 4, c1: 5)
+
+print("phương trinh bac 1 \(tinhPhuongTrinhBac1(a: 2, b: 3, c: 4, a1: 3, b1: 4, c1: 5))")
 
 //bai4
+
 func fibonaci(n: Int) {
     var f1 = 1
     var f2 = 0
@@ -66,10 +71,12 @@ func fibonaci(n: Int) {
     print("tong \(sum)")
 }
 
- fibonaci(n: 10)
+print("day fibonaci là: \(fibonaci(n: 10))")
 
 //    tinh sin cos
+
 func chuoiTaylor(x: Float , n: Int) -> Double {
+    
     let a: Int
     if n % 2 == 0 {
         a = 1
@@ -82,10 +89,12 @@ func chuoiTaylor(x: Float , n: Int) -> Double {
         }
     return Double(a) * Double(temp)
 }
+
 private func tinhChuoi(x: Float) -> (Int, Int) {
     var sin = 0
     var cos = 0
     var n: Int = 0
+    
     while abs(chuoiTaylor(x: x, n: n)) > 0.0001 {
         sin += Int(chuoiTaylor(x: x, n: n))
         n += 1
@@ -93,12 +102,15 @@ private func tinhChuoi(x: Float) -> (Int, Int) {
     cos = Int(sqrt(Double(1 - sin * sin)))
     return (sin, cos)
 }
+
 print(tinhChuoi(x: 0))
 
 // liêt ke so hanh phuc
+
 func isHappyNumber(num: Int) -> Bool {
     var number = num
     var arr: [Int] = []
+    
     while number > 0 {
         var surplus: Int = 0
         surplus = number % 10
@@ -120,6 +132,8 @@ func isHappyNumber(num: Int) -> Bool {
         return false
     }
 }
+print("liet ke so hanh phuc ")
+
 private func lietKe () {
     for i in 1...100 {
         if isHappyNumber(num: i) {
@@ -127,6 +141,7 @@ private func lietKe () {
         }
     }
 }
+
 lietKe()
 
 //tìm chuỗi con trong chuỗi mẹ
@@ -135,17 +150,17 @@ private func demChuoi(string: String, subString: String) -> Int {
     return string.components(separatedBy: subString).count - 1
 }
 
-demChuoi(string: "ababababababs", subString: "ab")
+print("số chuỗi con trong chuỗi mẹ: \(demChuoi(string: "abababaabsabsaab", subString: "ab"))")
     
 //bài tạp 6 : hoán vị
 
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-numbers.shuffle()
+print("đảo chuỗi: \(numbers.shuffled())")
 
 // bai tap 7
+
 func bearrayReplace(inputArray: [Int], elemToReplayce: Int, substitutionElem: Int) -> [Int] {
     var array = inputArray
-    
     for i in 0..<array.count {
         if array[i] == elemToReplayce {
             array[i] = substitutionElem
@@ -153,5 +168,7 @@ func bearrayReplace(inputArray: [Int], elemToReplayce: Int, substitutionElem: In
     }
     return array
 }
-bearrayReplace(inputArray: [1, 2, 3, 1, 1], elemToReplayce: 1, substitutionElem: 6)
+
+print("kết quả là: \(bearrayReplace(inputArray: [1, 2, 3, 1, 1], elemToReplayce: 1, substitutionElem: 6))")
+
 
