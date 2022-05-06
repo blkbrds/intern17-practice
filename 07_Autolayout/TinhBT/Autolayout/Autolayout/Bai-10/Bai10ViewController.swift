c6d702b2fe18b83b8eb145ba05d325f50d0ca19f
Tinh Bui T <tinh.bui@monstar-lab.com>
2022-05-06 15:11:52 +0700

    Fix error excercies Autolayout

--- 07_Autolayout/TinhBT/Autolayout/Autolayout/Bai-10/Bai10ViewController.swift
+++ 07_Autolayout/TinhBT/Autolayout/Autolayout/Bai-10/Bai10ViewController.swift
@@ -1,25 +1,25 @@
 
 import UIKit
 
-class Bai10ViewController: UIViewController {
+final class Bai10ViewController: UIViewController {
     
-    @IBOutlet weak var widthViewConstraint: NSLayoutConstraint!
-    @IBOutlet weak var widthSecondViewConstraint: NSLayoutConstraint!
-    @IBOutlet weak var widthRedViewConstraint: NSLayoutConstraint!
-    @IBOutlet weak var widthBlueViewConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var widthViewConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var widthSecondViewConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var widthRedViewConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var widthBlueViewConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-    @IBAction func tapGreenButtonTouchUpInside(_ sender: Any) {
+    @IBAction private func tapGreenButtonTouchUpInside(_ sender: Any) {
         widthViewConstraint.constant = 296
         widthSecondViewConstraint.constant = 148
         widthRedViewConstraint.constant = 148
         widthBlueViewConstraint.constant = 148
     }
     
-    @IBAction func tapOrangeButtonTouchUpInside(_ sender: Any) {
+    @IBAction private func tapOrangeButtonTouchUpInside(_ sender: Any) {
         widthViewConstraint.constant = 148
         widthSecondViewConstraint.constant = 296
         widthRedViewConstraint.constant = 148
@@ -27,14 +27,14 @@ class Bai10ViewController: UIViewController {
         
     }
     
-    @IBAction func tapRedButtonTouchUpInside(_ sender: Any) {
+    @IBAction private func tapRedButtonTouchUpInside(_ sender: Any) {
         widthViewConstraint.constant = 148
         widthSecondViewConstraint.constant = 148
         widthRedViewConstraint.constant = 296
         widthBlueViewConstraint.constant = 148
     }
     
-    @IBAction func tapBluebuttonTouchUpInside(_ sender: Any) {
+    @IBAction private func tapBluebuttonTouchUpInside(_ sender: Any) {
         widthViewConstraint.constant = 148
         widthSecondViewConstraint.constant = 148
         widthRedViewConstraint.constant = 148
--- 07_Autolayout/TinhBT/Autolayout/Autolayout/Bai-9/Bai9ViewController.swift
+++ 07_Autolayout/TinhBT/Autolayout/Autolayout/Bai-9/Bai9ViewController.swift
@@ -1,12 +1,12 @@
 
 import UIKit
 
-class Bai9ViewController: UIViewController {
+final class Bai9ViewController: UIViewController {
 
-    @IBOutlet weak var textLabel: UILabel!
-    @IBOutlet weak var inforLabel: UILabel!
-    @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet private weak var textLabel: UILabel!
+    @IBOutlet private weak var inforLabel: UILabel!
+    @IBOutlet private weak var scrollView: UIScrollView!
+    @IBOutlet private weak var imageView: UIImageView!
     
     var index = 0
     
@@ -33,8 +33,8 @@ class Bai9ViewController: UIViewController {
         inforLabel.text = array[index].message
         textLabel.text = "\(index + 1)/8"
     }
-    
 }
+
 class Cat {
     var image: String
     var message: String
@@ -44,6 +44,7 @@ class Cat {
         self.message = message
     }
 }
+
 let array: [Cat] = [Cat(image: "cats", message: "m Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survmake a typeince the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesm Ipsum is simply dummy text of the"),
                     Cat(image: "cat", message: "m Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survmake a typeince the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesm Ipsum is simply dummy text of the"),
                     Cat(image: "cats", message: "m Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survmake a typeince the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesm Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has bestry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to vived not only five centuries, but also the leap into electronic"),
