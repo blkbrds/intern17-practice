86e0aff29f2b209417811fc5ac776c46151c3ee9
Phong Huynh N <phong.huynh@monstar-lab.com>
2022-05-11 11:32:39 +0700

    Config Ui Detail

---
+++ BaiTapTongHop/PhongHuynh/FinalProject/FinalProject/View/Controller/Detail/DetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DetailViewController.swift
+//  FinalProject
+//
+//  Created by Phong Huynh N. VN.Danang on 5/11/22.
+//  Copyright © 2022 Asiantech. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailView: UIView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
