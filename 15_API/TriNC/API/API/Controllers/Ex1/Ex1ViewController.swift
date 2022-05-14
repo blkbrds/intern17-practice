1affd84f5981cb7ada6872e31c4fbe9017594d30
Tri Nguyen C <tri.nguyen@monstar-lab.com>
2022-05-14 10:42:02 +0700

    API

---
+++ 15_API/TriNC/API/API/Controllers/Ex1/Ex1ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  Ex1ViewController.swift
+//  API
+//
+//  Created by tri.nguyen on 14/05/2022.
+//
+
+import UIKit
+
+final class Ex1ViewController: UIViewController {
+
+    // MARK: - Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Ex1"
+    }
+}
---
+++ 15_API/TriNC/API/API/Controllers/Ex2/Ex2ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  Ex2ViewController.swift
+//  API
+//
+//  Created by tri.nguyen on 14/05/2022.
+//
+
+import UIKit
+
+final class Ex2ViewController: UIViewController {
+
+    // MARK: - Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Ex2"
+    }
+}
