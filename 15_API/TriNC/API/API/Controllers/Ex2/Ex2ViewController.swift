719a25d96be8baa7ceda84dbef76c7cd7106e4f5
Tri Nguyen C <tri.nguyen@monstar-lab.com>
2022-05-16 09:08:11 +0700

    API

--- 15_API/TriNC/API/API/Controllers/Ex2/Ex2ViewController.swift
+++ 15_API/TriNC/API/API/Controllers/Ex2/Ex2ViewController.swift
@@ -2,7 +2,7 @@
 //  Ex2ViewController.swift
 //  API
 //
-//  Created by tri.nguyen on 14/05/2022.
+//  Created by tri.nguyen on 16/05/2022.
 //
 
 import UIKit
@@ -12,6 +12,5 @@ final class Ex2ViewController: UIViewController {
     // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Ex2"
     }
 }
