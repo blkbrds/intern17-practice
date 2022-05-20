a6c14fa23c922acb79b4a1b86a9d9b415528c303
Tinh Bui T <tinh.bui@monstar-lab.com>
2022-05-20 08:42:49 +0700

    Fix comment excersice MVC

--- 08_MVC/TinhBui/BaiTapMVC/BaiTapMVC/Controller/ViewController.swift
+++ 08_MVC/TinhBui/BaiTapMVC/BaiTapMVC/Controller/ViewController.swift
@@ -1,42 +1,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     
     var so1: Int!
     var so2: Int!
     var dau: String!
 
-    @IBOutlet weak var ketquaTextField: UITextField!
+    @IBOutlet private weak var ketquaTextField: UITextField!
+    
+    var caculator = Calculator()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
     
-    @IBAction func nutBang(_ sender: Any) {
+    @IBAction private func nutBang(_ sender: Any) {
         let so2String = ketquaTextField.text ?? "0"
         so2 = Int(so2String)
         
         var ketqua = 0
             
         if dau == "+" {
-            ketqua = so1 + so2
+            ketqua = caculator.cong(a: so1, b: so2)
         }
         if dau == "-" {
-            ketqua = so1 - so2
+            ketqua = caculator.tru(a: so1, b: so2)
         }
         if dau == "x" {
-            ketqua = so1 * so2
+            ketqua = caculator.nhan(a: so1, b: so2)
         }
         if dau == "/" {
-            ketqua = so1 / so2
+            ketqua = caculator.chia(a: so1, b: so2)
         }
         
         ketquaTextField.text = "\(ketqua)"
     }
     
-    @IBAction func nutCong(_ sender: Any) {
+    @IBAction private func nutCong(_ sender: Any) {
         let so1String = ketquaTextField.text ?? "0"
         so1 = Int(so1String)
         
@@ -44,7 +45,7 @@ class ViewController: UIViewController {
         dau = "+"
     }
 
-    @IBAction func nutTru(_ sender: Any) {
+    @IBAction private func nutTru(_ sender: Any) {
         let so1String = ketquaTextField.text ?? "0"
         so1 = Int(so1String)
         
@@ -52,7 +53,7 @@ class ViewController: UIViewController {
         dau = "-"
     }
     
-    @IBAction func nutNhan(_ sender: Any) {
+    @IBAction private func nutNhan(_ sender: Any) {
         let so1String = ketquaTextField.text ?? "0"
         so1 = Int(so1String)
         
@@ -60,7 +61,7 @@ class ViewController: UIViewController {
         dau = "x"
     }
     
-    @IBAction func nutChia(_ sender: Any) {
+    @IBAction private func nutChia(_ sender: Any) {
         let so1String = ketquaTextField.text ?? "0"
         so1 = Int(so1String)
         
@@ -68,74 +69,74 @@ class ViewController: UIViewController {
         dau = "/"
     }
     
-    @IBAction func nutXoa(_ sender: Any) {
+    @IBAction private func nutXoa(_ sender: Any) {
         ketquaTextField.text = ""
     }
     
-    @IBAction func nut0(_ sender: Any) {
+    @IBAction private func nut0(_ sender: Any) {
         var kq: String = ketquaTextField.text ?? ""
         kq += "0"
         
         ketquaTextField.text = kq
     }
     
-    @IBAction func nut1(_ sender: Any) {
+    @IBAction private func nut1(_ sender: Any) {
         var kq: String = ketquaTextField.text ?? ""
         kq += "1"
         
         ketquaTextField.text = kq
     }
     
-    @IBAction func nut2(_ sender: Any) {
+    @IBAction private func nut2(_ sender: Any) {
         var kq: String = ketquaTextField.text ?? ""
         kq += "2"
         
         ketquaTextField.text = kq
     }
     
-    @IBAction func nut3(_ sender: Any) {
+    @IBAction private func nut3(_ sender: Any) {
         var kq: String = ketquaTextField.text ?? ""
         kq += "3"
         
         ketquaTextField.text = kq
     }
     
-    @IBAction func nut4(_ sender: Any) {
+    @IBAction private func nut4(_ sender: Any) {
         var kq: String = ketquaTextField.text ?? ""
         kq += "4"
         
         ketquaTextField.text = kq
     }
     
-    @IBAction func nut5(_ sender: Any) {
+    @IBAction private func nut5(_ sender: Any) {
         var kq: String = ketquaTextField.text ?? ""
         kq += "5"
         
         ketquaTextField.text = kq
     }
     
-    @IBAction func nut6(_ sender: Any) {
+    @IBAction private func nut6(_ sender: Any) {
         var kq: String = ketquaTextField.text ?? ""
         kq += "6"
         
         ketquaTextField.text = kq
     }
     
-    @IBAction func nut7(_ sender: Any) {
+    @IBAction private func nut7(_ sender: Any) {
         var kq: String = ketquaTextField.text ?? ""
         kq += "7"
         
         ketquaTextField.text = kq
     }
     
-    @IBAction func nut8(_ sender: Any) {
+    @IBAction private func nut8(_ sender: Any) {
         var kq: String = ketquaTextField.text ?? ""
         kq += "8"
         
         ketquaTextField.text = kq
     }
     
-    @IBAction func nut9(_ sender: Any) {
+    @IBAction private func nut9(_ sender: Any) {
         var kq: String = ketquaTextField.text ?? ""
         kq += "9"
         
---
+++ 08_MVC/TinhBui/BaiTapMVC/BaiTapMVC/Models/Calculator.swift
@@ -0,0 +1,27 @@
+//
+//  Calculator.swift
+//  BaiTapMVC
+//
+//  Created by Tinh Bui T. VN.Danang on 5/20/22.
+//
+
+import Foundation
+
+final class Calculator {
+    
+    func cong(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    func tru(a: Int, b:Int) -> Int {
+        return a - b
+    }
+    
+    func nhan(a: Int, b: Int) -> Int {
+        return a * b
+    }
+    
+    func chia(a: Int, b: Int) -> Int {
+        return a / b
+    }
+}
