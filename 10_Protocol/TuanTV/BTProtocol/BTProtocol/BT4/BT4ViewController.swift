a1885b9e1033eb940459ce981798341e4495dc64
Tuan Tran V <tuan.tran@monstar-lab.com>
2022-05-25 15:00:01 +0700

    Done BT4 Protocol

--- 10_Protocol/TuanTV/BTProtocol/BTProtocol/BT4/BT4ViewController.swift
+++ 10_Protocol/TuanTV/BTProtocol/BTProtocol/BT4/BT4ViewController.swift
@@ -7,7 +7,22 @@
 
 import UIKit
 
-final class BT4ViewController: UIViewController {
+final class BT4ViewController: UIViewController, UISearchBarDelegate {
+    
+    //MARK: Property
+    var cityList: [String] = ["Hoà Bình", "Sơn La", "Điện Biên", "Lai Châu", "Lào Cai", "Yên Bái", "Phú Thọ", "Hà Giang", "Tuyên Quang", "Cao Bằng", "Bắc Cạn", "Thái Nguyên", "Lạng Sơn", "Bắc Giang", "Quảng Ninh", "Hà Nội", "Bắc Ninh", "Hà Nam", "Hải Dương", "Hải Phòng", "Hưng Yên", "Nam Định", "Thái Bình", "Vĩnh Phúc", "Ninh Bình", "Thanh Hoá"]
+    var tempCityList: [String] = [] {
+        didSet {
+            // Khi thay đổi du lieu temp thi chuyen qua set string Display
+            stringDisplay = ""
+            for city in tempCityList {
+                stringDisplay = stringDisplay + "- \(city)\n"
+            }
+            listTextView.text = stringDisplay
+        }
+    }
+    var stringDisplay: String = ""
+    
     //MARK: - IBOutlets
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var listTextView: UITextView!
@@ -15,11 +30,23 @@ final class BT4ViewController: UIViewController {
     //MARK: - IBActions
     @IBAction func cancelTapped(_ sender: UIButton) {
         print("cancel")
+        tempCityList = cityList
+        searchBar.text = ""
     }
     
     //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.delegate = self
+        tempCityList = cityList
     }
-
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
+        if searchText.isEmpty == false {
+            tempCityList = cityList.filter({$0.contains(searchText)})
+        } else {
+            tempCityList = cityList
+        }
+    }
 }
--- 10_Protocol/TuanTV/BTProtocol/BTProtocol/SceneDelegate.swift
+++ 10_Protocol/TuanTV/BTProtocol/BTProtocol/SceneDelegate.swift
@@ -16,6 +16,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
+        let viewController = BT4ViewController()
+        window?.rootViewController = viewController
         window?.makeKeyAndVisible()
         
     }
