5cbef83580bd4698d0be456c3359a0f2479f3bfa
Tuan Tran V <tuan.tran@monstar-lab.com>
2022-05-25 10:26:24 +0700

    finish UI in XIB

---
+++ 10_Protocol/TuanTV/BTProtocol/BTProtocol/BT4/BT4ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BT4ViewController.swift
+//  BTProtocol
+//
+//  Created by Tuan Tran V. VN.Danang on 5/25/22.
+//
+
+import UIKit
+
+final class BT4ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
