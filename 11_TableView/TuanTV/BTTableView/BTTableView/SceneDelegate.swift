da5d58e487d874954ba09c397cf6306dd5b1176e
Tuan Tran V <tuan.tran@monstar-lab.com>
2022-05-26 10:40:51 +0700

    set up no storyboard in BT tableview

---
+++ 11_TableView/TuanTV/BTTableView/BTTableView/SceneDelegate.swift
@@ -0,0 +1,39 @@
+//
+//  SceneDelegate.swift
+//  BTTableView
+//
+//  Created by Tuan Tran V. VN.Danang on 5/26/22.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScence = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScence)
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+        
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+        
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        
+    }
+}
