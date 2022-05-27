cf63518507e1f2db17f803087562cb1f32cc3df7
Phong Huynh N <phong.huynh@monstar-lab.com>
2022-05-27 15:54:19 +0700

    Config ui profile

---
+++ BaiTapTongHop/PhongHuynh/FinalProject/FinalProject/View/Controller/Profile/Cell/InterestListCell.swift
@@ -0,0 +1,24 @@
+//
+//  InterestListCell.swift
+//  FinalProject
+//
+//  Created by Phong Huynh N. VN.Danang on 5/27/22.
+//  Copyright © 2022 Asiantech. All rights reserved.
+//
+
+import UIKit
+
+class InterestListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ BaiTapTongHop/PhongHuynh/FinalProject/FinalProject/View/Controller/Profile/Cell/LogOutCell.swift
@@ -0,0 +1,24 @@
+//
+//  LogOutCell.swift
+//  FinalProject
+//
+//  Created by Phong Huynh N. VN.Danang on 5/27/22.
+//  Copyright © 2022 Asiantech. All rights reserved.
+//
+
+import UIKit
+
+class LogOutCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
