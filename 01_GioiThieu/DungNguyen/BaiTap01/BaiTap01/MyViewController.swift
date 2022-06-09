1637e45d23509ac8bce0db5b7cacac6bb9bfd604
Dung Nguyen T.T. [3] <dung.nguyen3@monstar-lab.com>
2022-06-09 10:03:15 +0700

    Fix comment exercise 01

--- 01_GioiThieu/DungNguyen/BaiTap01/BaiTap01/MyViewController.swift
+++ 01_GioiThieu/DungNguyen/BaiTap01/BaiTap01/MyViewController.swift
@@ -12,30 +12,17 @@ class MyViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .yellow
-
-        // Do any additional setup after loading the view.
     }
     
     func getRandomColor() -> UIColor {
          //Generate between 0 to 1
-         let red:CGFloat = CGFloat(drand48())
-         let green:CGFloat = CGFloat(drand48())
-         let blue:CGFloat = CGFloat(drand48())
+         let red: CGFloat = CGFloat(drand48())
+         let green: CGFloat = CGFloat(drand48())
+         let blue: CGFloat = CGFloat(drand48())
 
-         return UIColor(red:red, green: green, blue: blue, alpha: 1.0)
+         return UIColor(red: red, green: green, blue: blue, alpha: 1.0)
     }
     @IBAction func btTap(_ sender: Any) {
-        
         self.view.backgroundColor = getRandomColor()
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
