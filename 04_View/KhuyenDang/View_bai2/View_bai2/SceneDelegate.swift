9cd3324186623afd02c729922404c73fc2db265a
Khuyen Dang T.T <khuyen.dang@monstar-lab.com>
2022-06-15 15:56:44 +0700

    Implement exercise view_bai2

---
+++ 04_View/KhuyenDang/View_bai2/View_bai2/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  View_bai2
+//
+//  Created by Khuyen Dang T.T. VN.Danang on 6/14/22.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+                let window = UIWindow(windowScene: windowScene)
+                window.rootViewController = ArrayUserViewController()
+                self.window = window
+                window.makeKeyAndVisible()
+                window.backgroundColor = .white
+    }
+}
+
