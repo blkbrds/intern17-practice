a0b0488b5e5c8d8a3ce6c9701a60d3a9611e3576
Tu Le C. [2] <tu.le2@monstar-lab.com>
2022-06-16 10:01:19 +0700

    Implement exercise 01 customView

---
+++ 05_CustomView/TuLe/CustomView/CustomView/MyAvatar.swift
@@ -0,0 +1,61 @@
+import UIKit
+
+protocol UserViewDelegate {
+    func didTap(view: MyAvatar, count: Int)
+}
+
+class MyAvatar: UIView {
+    
+    var avatarImageView: UIImageView?
+    var nameLabel: UILabel?
+    var markButton: UIButton?
+    var count: Int = 0
+    var delegate: UserViewDelegate?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .blue
+        
+        avatarImageView = UIImageView(image: UIImage(named: "avatar1"))
+        avatarImageView!.frame = CGRect(x: 0,
+                                        y: 0,
+                                        width: frame.size.width,
+                                        height: frame.size.height*4/5)
+        avatarImageView!.contentMode = .scaleAspectFill
+        self.addSubview(avatarImageView!)
+        
+        nameLabel = UILabel(frame: CGRect(x: 0,
+                                          y: frame.size.height*4/5,
+                                          width: frame.size.width,
+                                          height: frame.size.height/5))
+        nameLabel!.text = "Tú Đeep Try"
+        nameLabel!.backgroundColor = .lightGray
+        nameLabel!.textAlignment = .center
+        nameLabel!.textColor = .blue
+        self.addSubview(nameLabel!)
+        
+        markButton = UIButton(frame: CGRect(x: 0,
+                                            y: 0,
+                                            width: frame.size.width,
+                                            height: frame.size.height))
+        markButton!.backgroundColor = .clear
+        markButton?.addTarget(self, action: #selector(tap), for: .touchUpInside)
+        self.addSubview(markButton!)
+    }
+    
+    @objc func tap() {
+        count += 1
+        nameLabel?.text = "\(count)"
+        
+        guard let delegate = delegate else {
+            return
+        }
+
+        delegate.didTap(view: self, count: count)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
