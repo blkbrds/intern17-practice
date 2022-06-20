afbf68cb9e7fa342a3df850940f58c6e7edafa55
Khuyen Dang T.T <khuyen.dang@monstar-lab.com>
2022-06-20 15:21:16 +0700

    Fix comment view_bai5

--- 04_View/KhuyenDang/View_bai5/View_bai5/View_bai5/CustomSliderViewController.swift
+++ 04_View/KhuyenDang/View_bai5/View_bai5/View_bai5/CustomSliderViewController.swift
@@ -10,11 +10,11 @@ import UIKit
 final class CustomSliderViewController: UIViewController {
     
     //MARK: - IBOutlets
-    @IBOutlet weak var greenColorView: UIView!
-    @IBOutlet weak var orangeColorView: UIView!
-    @IBOutlet weak var blueColorView: UIView!
-    @IBOutlet weak var indexLabel: UILabel!
-    @IBOutlet weak var containerView: UIView!
+    @IBOutlet private weak var greenColorView: UIView!
+    @IBOutlet private weak var orangeColorView: UIView!
+    @IBOutlet private weak var blueColorView: UIView!
+    @IBOutlet private weak var indexLabel: UILabel!
+    @IBOutlet private weak var containerView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +24,7 @@ final class CustomSliderViewController: UIViewController {
     
     // MARK: - Private methods
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
+        super.touchesMoved(touches, with: event)
         if let touch = touches.first {
             if touch.view == orangeColorView {
                 let location = touch.location(in: containerView)
@@ -37,8 +37,7 @@ final class CustomSliderViewController: UIViewController {
                     orangeColorView.center.y = location.y
                 }
                 blueColorView.frame = CGRect(x: greenColorView.frame.origin.x, y: orangeColorView.center.y, width: greenColorView.frame.width, height: (greenColorView.frame.height - orangeColorView.center.y))
-                
-                var percent = Int((blueColorView.frame.height / greenColorView.frame.height) * 100)
+                let percent = Int((blueColorView.frame.height / greenColorView.frame.height) * 100)
                 indexLabel.text = "\(percent)"
             }
         }
