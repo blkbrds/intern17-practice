040ed36b326f25dda1017d2996dfb25223ec39b3
Khuyen Dang T.T <khuyen.dang@monstar-lab.com>
2022-06-20 15:10:24 +0700

    Fix comment view_bai3

--- 04_View/KhuyenDang/View_bai3/View_bai3/View_bai3/LoginViewController.swift
+++ 04_View/KhuyenDang/View_bai3/View_bai3/View_bai3/LoginViewController.swift
@@ -41,6 +41,7 @@ final class LoginViewController: UIViewController {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
         usernameTextField.resignFirstResponder()
         passwordTextField.resignFirstResponder()
     }
