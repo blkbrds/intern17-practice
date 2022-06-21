76f1f75e681f44f1865a216ef668e35d63e7de90
Dung Nguyen T.T. [3] <dung.nguyen3@monstar-lab.com>
2022-06-21 17:05:40 +0700

    Implement exercise CustomView_3

--- 05_CustomView/DungNguyen/CustomView/CustomView/SceneDelegate.swift
+++ 05_CustomView/DungNguyen/CustomView/CustomView/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
      
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = MySliderViewController()
+        window.rootViewController = BadgeNumberViewController()
         self.window = window
         window.makeKeyAndVisible()
     }
