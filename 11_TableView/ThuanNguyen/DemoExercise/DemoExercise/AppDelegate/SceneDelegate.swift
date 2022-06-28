de4c6adb28dc70266de2edaba37c7bbe06293b31
Thuan Nguyen <thuan.nguyen@monstar-lab.com>
2022-06-28 17:12:49 +0700

    Do Demo Exercise Mentor in TableView

---
+++ 11_TableView/ThuanNguyen/DemoExercise/DemoExercise/AppDelegate/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  DemoExercise
+//
+//  Created by thuan.nguyen on 28/06/2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        let vc = HomeViewController()
+        let navi = UINavigationController(rootViewController: vc)
+        window.rootViewController = navi
+        self.window = window
+        window.makeKeyAndVisible()    }
+
+}
+
