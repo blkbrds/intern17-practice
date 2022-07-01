3f4c8a5739406e069a43095d42b4ee3ac350b4e5
Tu Le C. [2] <tu.le2@monstar-lab.com>
2022-07-01 13:53:33 +0700

    Fix commemt exercise Autolayout

--- 07_Autolayout/TuLe/Autolayout/Autolayout/BT10/BT10ViewController.swift
+++ 07_Autolayout/TuLe/Autolayout/Autolayout/BT10/BT10ViewController.swift
@@ -7,50 +7,47 @@
 
 import UIKit
 
+struct Define {
+    static let bigView: CGFloat = 297
+    static let smallView: CGFloat = 148
+}
+
 final class BT10ViewController: UIViewController {
-    @IBOutlet private weak var yellowView: UIView!
-    @IBOutlet private weak var greenView: UIView!
-    @IBOutlet private weak var violetView: UIView!
-    @IBOutlet private weak var orangeView: UIView!
     @IBOutlet private weak var heightVioletConstraint: NSLayoutConstraint!
     @IBOutlet private weak var heightYellowConstraint: NSLayoutConstraint!
     @IBOutlet private weak var heightOrangeConstraint: NSLayoutConstraint!
     @IBOutlet private weak var heightGreenConstraint: NSLayoutConstraint!
-
-    
-    var bigView = UIView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        bigView = greenView
     }
     
     @IBAction private func handleSingleTapGreenView(_ sender: Any) {
-        heightGreenConstraint.constant = 297
-        heightYellowConstraint.constant = 148
-        heightVioletConstraint.constant = 148
-        heightOrangeConstraint.constant = 148
+        heightGreenConstraint.constant = Define.bigView
+        heightYellowConstraint.constant = Define.smallView
+        heightVioletConstraint.constant = Define.smallView
+        heightOrangeConstraint.constant = Define.smallView
     }
     
     
     @IBAction private func handleSingleTapVioletView(_ sender: Any) {
-        heightVioletConstraint.constant = 297
-        heightYellowConstraint.constant = 148
-        heightGreenConstraint.constant = 148
-        heightOrangeConstraint.constant = 148
+        heightVioletConstraint.constant = Define.bigView
+        heightYellowConstraint.constant = Define.smallView
+        heightGreenConstraint.constant = Define.smallView
+        heightOrangeConstraint.constant = Define.smallView
     }
     
     @IBAction private func handleSingleTapYellowView(_ sender: Any) {
-        heightYellowConstraint.constant = 297
-        heightVioletConstraint.constant = 148
-        heightGreenConstraint.constant = 148
-        heightOrangeConstraint.constant = 148
+        heightYellowConstraint.constant = Define.bigView
+        heightVioletConstraint.constant = Define.smallView
+        heightGreenConstraint.constant = Define.smallView
+        heightOrangeConstraint.constant = Define.smallView
     }
     
     @IBAction private func handleSingleTapOrangeView(_ sender: Any) {
-        heightOrangeConstraint.constant = 297
-        heightYellowConstraint.constant = 148
-        heightGreenConstraint.constant = 148
-        heightVioletConstraint.constant = 148
+        heightOrangeConstraint.constant = Define.bigView
+        heightYellowConstraint.constant = Define.smallView
+        heightGreenConstraint.constant = Define.smallView
+        heightVioletConstraint.constant = Define.smallView
     }
 }
