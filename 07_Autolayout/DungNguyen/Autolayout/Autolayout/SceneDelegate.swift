22fdfaa000969633701dde463ee61e54a504ffd6
Dung Nguyen T.T. [3] <dung.nguyen3@monstar-lab.com>
2022-07-04 16:49:54 +0700

    Fix comment AutoLayout

--- 07_Autolayout/DungNguyen/Autolayout/Autolayout/SceneDelegate.swift
+++ 07_Autolayout/DungNguyen/Autolayout/Autolayout/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = Bai10ViewController()
+        window.rootViewController = Bai14ViewController()
         self.window = window
         window.makeKeyAndVisible()
     }
